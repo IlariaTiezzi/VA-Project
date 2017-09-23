cad4c6b45af7a0012379e487c1ac5cb4a71c21ce
Ilaria Tiezzi <ilaria.tiezzi@gmail.com>
2017-09-23 16:02:35 +0200

    Improving all cumulative line chart components, update layout and style

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,3 +1,9 @@
+
+// Global variables
+var c20c = d3.scale.category20c().range();
+var colors = [c20c[3],c20c[2], c20c[1], c20c[0]];
+var formatSuffix = d3.format(".2s");
+
 // Create a heatmap  to display the number of users per hours of the friday
 d3.json("assets/data/we-h-area-fri.json", 
 
@@ -6,9 +12,7 @@ d3.json("assets/data/we-h-area-fri.json",
 
 		var margin = 30;
 		var w = 200;
-		var h = 470;
-		var colors = ["#c6dbef", "#9ecae1", "#6baed6", "#3182bd"];
-		var formatSuffix = d3.format(".2s");
+		var h = 470;		
 		var extentValue = d3.extent(data, function(d){ return d.users});
 
         // Set the names of areas
@@ -98,7 +102,7 @@ d3.json("assets/data/we-h-area-fri.json",
 
         		// Updates the value of the second paragraph
         		var labelValue = d3.select("#heatmap-value-fri")
-        			.text(d.value)
+        			.text(formatSuffix(d.value))
         			.style("font-size", "20px")
         			.style("font-weight", "bold");
 
@@ -166,8 +170,6 @@ d3.json("assets/data/we-h-area-sat.json",
         var margin = 30;
         var w = 200;
         var h = 470;
-        var colors = ["#c6dbef", "#9ecae1", "#6baed6", "#3182bd"];
-        var formatSuffix = d3.format(".2s");
         var extentValue = d3.extent(data, function(d){ return d.users});
 
         // Set the names of areas
@@ -257,7 +259,7 @@ d3.json("assets/data/we-h-area-sat.json",
 
                 // Updates the value of the second paragraph
                 var labelValue = d3.select("#heatmap-value-sat")
-                    .text(d.value)
+                    .text(formatSuffix(d.value))
                     .style("font-size", "20px")
                     .style("font-weight", "bold");
 
@@ -325,8 +327,6 @@ d3.json("assets/data/we-h-area-sun.json",
         var margin = 30;
         var w = 200;
         var h = 470;
-        var colors = ["#c6dbef", "#9ecae1", "#6baed6", "#3182bd"];
-        var formatSuffix = d3.format(".2s");
         var extentValue = d3.extent(data, function(d){ return d.users});
 
         // Set the names of areas
@@ -412,11 +412,11 @@ d3.json("assets/data/we-h-area-sun.json",
 
                 // Updates the value of the first paragraph
                 var labelTime = d3.select("#heatmap-time-sun")
-                    .text(d.area + " " + d.hour);
+                    .text(d.area + " " + d.hour);            
 
                 // Updates the value of the second paragraph
                 var labelValue = d3.select("#heatmap-value-sun")
-                    .text(d.value)
+                    .text(formatSuffix(d.value))
                     .style("font-size", "20px")
                     .style("font-weight", "bold");
 
@@ -472,5 +472,79 @@ d3.json("assets/data/we-h-area-sun.json",
             .style("fill", "#0192b5")
             .style("font-family", "sans-serif");        
         
+    }
+);
+
+// Create a cumulative line chart to compare the number of movement per hours of the day
+d3.json("assets/data/we-mov.json", 
+
+    function(error, data) {
+        if (error) {console.log(error);} 
+
+        var setColor = [colors[0],colors[2], colors[3]];
+
+        // Set the name of the days
+        var setDay = function(d){
+            var day;
+            if (d.day === "06") { return day = "Friday";}
+            else if (d.day === "07") { return day = "Saturday";}
+            else { return day = "Sunday";}
+            return day;
+        }
+
+        // Define a new data array and assign the name of days
+        var dataset = d3.nest()
+            .key(function(d){return setDay(d);})
+            .entries(data)
+            .map(function(d) {
+                return {
+                    key: d.key,
+                    values: d.values.map(function(p){return [p.hour, p.users];})
+                }
+            });      
+
+        console.log(dataset);
+
+        // Create a SVG element inside the DIV with ID #linechart        
+        var svg = d3.select("#linechart")
+            .append("svg")
+            .attr("width", 600)
+            .attr("height", 600);        
+
+        // Add a cumulative line chart
+        nv.addGraph(function() {
+        var chart = nv.models.cumulativeLineChart()            
+            .x(function(d) { return d[0] })
+            .y(function(d) { return d[1]/100}) // 100% is 1.00
+            .color(setColor)          // Select the range of colors                   
+            .showControls(false)    // Don't show the controls
+            .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+            .showYAxis(true)        //Show the y-axis
+            .showXAxis(true)        //Show the x-axis
+            .useInteractiveGuideline(true);
+
+        // Define the label and displays all data in the x axis 
+        chart.xAxis
+            .axisLabel("Hours")
+            .ticks(16);       
+
+        // Define the format of number in the y axis
+        chart.yAxis
+            .tickFormat(d3.format(',.1%')); 
+
+        // Add the data to chart
+        d3.select('#linechart svg')
+            .datum(dataset)
+            .call(chart);
+
+        // Resize chart according to the size of the window
+        nv.utils.windowResize(chart.update);
+
+        return chart;
+      });
+
+
+
+
     }
 );
